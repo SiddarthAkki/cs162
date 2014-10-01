ca15277059448d6d5a1f73d1efe14fb6110d8570
Emily Xie <vagrant@cs162-fall2014>
2014-10-01 09:13:38 +0000

    add waiting_time into struct thread

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -92,6 +92,8 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    
+      int waiting_time;                 /* End of wait time */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
