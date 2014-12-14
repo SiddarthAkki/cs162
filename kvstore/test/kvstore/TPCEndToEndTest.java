9d62915f15d331c65e1918559dd0b8b7819790f4
Emily Xie <vagrant@cs162-fall2014>
2014-12-14 07:03:13 +0000

    uncommented test

--- kvstore/test/kvstore/TPCEndToEndTest.java
+++ kvstore/test/kvstore/TPCEndToEndTest.java
@@ -23,56 +23,120 @@ public class TPCEndToEndTest extends TPCEndToEndTemplate {
     @Test(timeout = kTimeoutQuick)
     public void testPutandGet() {
         try {
-            client.put("sup", "dawg");
+            client.put(KEY1, "dawg");
         } catch (KVException e) {
             fail("should not have raised exception");
         }
         String val = null;
         try {
-            val = client.get("sup");
+            val = client.get(KEY1);
         } catch (KVException e) {
             fail("should not have raised exception");
         }
         assertNotNull(val);
         assertTrue(val.equals("dawg"));
+        
+        try {
+            client.put(KEY1, "newdawg");
+        } catch (KVException e) {
+            fail("should not have raised exception");
+        }
+        try {
+            val = client.get(KEY1);
+        } catch (KVException e) {
+            fail("should not have raised exception");
+        }
+        assertNotNull(val);
+        assertTrue(val.equals("newdawg"));
     }
     
     
     @Test(timeout = kTimeoutQuick)
     public void testDel() {
         try {
-            client.put("Anu", "EXTREME!!!!");
+            client.put(KEY2, "EXTREME!!!!");
         } catch (KVException e) {
             fail("should not have raised exception");
         }
         try {
-            client.del("Anu");
+            client.del(KEY2);
         } catch (KVException e) {
             fail("should not have raised exception");
         }
         try {
-            client.get("Anu");
+            client.get(KEY2);
             fail("Key should have been deleted");
         } catch (KVException e) {
             assertTrue(ERROR_NO_SUCH_KEY.equals(e.getKVMessage().getMessage()));
         }
     }
 	
-    /*
+    
     @Test(timeout = kTimeoutQuick)
     public void testErrors() {
         try {
-            client.get("ANUISSOEXTREME!!!");
+            client.get(KEY3);
             fail("invalid key");
         } catch (KVException e) {
             assertTrue(ERROR_NO_SUCH_KEY.equals(e.getKVMessage().getMessage()));
         }
+        
         try {
-            client.del("PEANUTBUTTERBANANAS");
+            client.del(KEY4);
             fail("invalid key");
         } catch (KVException e) {
             assertTrue(ERROR_NO_SUCH_KEY.equals(e.getKVMessage().getMessage()));
+        }
+        
+        try {
+            client.put(KEY4, null);
+            fail("Need to throw exception on invalid value");
+        } catch (KVException e) {
+            assertTrue(ERROR_INVALID_VALUE.equals(e.getKVMessage().getMessage()));
+        }
+        
+        try {
+            client.put(null, "orchid");
+            fail("Need to throw exception on invalid key");
+        } catch (KVException e) {
+            assertTrue(ERROR_INVALID_KEY.equals(e.getKVMessage().getMessage()));
+        }
+        
+        try {
+            client.put(KEY4, "");
+            fail("Need to throw exception on invalid value");
+        } catch (KVException e) {
+            assertTrue(ERROR_INVALID_VALUE.equals(e.getKVMessage().getMessage()));
+        }
+        
+        try {
+            client.put("", "orchid");
+            fail("Need to throw exception on invalid key");
+        } catch (KVException e) {
+            assertTrue(ERROR_INVALID_KEY.equals(e.getKVMessage().getMessage()));
+        }
+        
+        try {
+            client.get("");
+            fail("Need to throw exception on invalid key");
+        } catch (KVException e) {
+            assertTrue(ERROR_INVALID_KEY.equals(e.getKVMessage().getMessage()));
         }
     }
-    */
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
